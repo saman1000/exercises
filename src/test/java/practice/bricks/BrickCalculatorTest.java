0166d1e4f2b873c10166fb47ad291be3d7996539
saman1000 <saman1000@gmail.com>
2021-02-21 00:59:04 -0500

    added simple exercise as maven project

---
+++ src/test/java/practice/bricks/BrickCalculatorTest.java
@@ -0,0 +1,25 @@
+package practice.bricks;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class BrickCalculatorTest {
+
+    private final BrickCalculator brickCalculator = new BrickCalculator();
+
+    @ParameterizedTest
+    @CsvSource({
+            "3, 1, 8, true",
+            "3, 1, 9, false",
+            "3, 2, 10, true",
+            "3, 6, 22, true",
+            "8, 0, 7, true",
+            "1, 6, 22, false",
+            "0, 1, 5, true",
+            "2, 1, 8, false",
+            "4, 3000, 15003, true"
+    })
+    public void testEnoughBricks(int small, int big, int goal, boolean enough) {
+        assert brickCalculator.enoughBricks(small, big, goal) == enough;
+    }
+}
